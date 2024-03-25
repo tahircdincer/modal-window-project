0d91c546e6fd0cd0e2b22f85e0c55fd6533df6e3
Tahir Dincer <coskun.dincer2014@gmail.com>
2024-03-26 01:52:46 +0300

    Variables declared for shorter code

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const modal = document.querySelector(".modal ");
+const openModal = document.querySelector(" .show-modal ");
+const overlay = document.querySelector(".overlay");
+const closeModal = document.querySelector(" .close-modal ");
